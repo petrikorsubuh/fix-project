0bea4bd5a98dcc18826dbc0769cc206cc121a3a7
zizonk <afifnurazizi@gmail.com>
2019-02-08 02:06:12 +0900

    chore:change production.py,local.py,and models.py

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,8 +3,9 @@ from ..base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'root',
-        'USER': '29494*',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': '29494*',
         'HOST': 'localhost',  # set in docker-compose.yml
         'PORT': 3306  # default postgres port
     }
--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,8 @@ from ..base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'root',
+        'NAME': 'mysql',
+        'USER': 'root',
         'USER': '29494*',
         'HOST': 'localhost',  # set in docker-compose.yml
         'PORT': 3306  # default postgres port
