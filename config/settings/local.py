e4c380495098f320da0d210ca304fd2c04daa5c4
zizonk <afifnurazizi@gmail.com>
2019-04-16 06:43:51 +0900

    chore:change local database

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,12 +1,19 @@
 from ..base import *
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'qcount',
+#         'USER': 'root',
+#         'PASSWORD': '29494*',
+#         'HOST': 'localhost',  # set in docker-compose.yml
+#         'PORT': 3306  # default postgres port
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'qcount',
-        'USER': 'root',
-        'PASSWORD': '29494*',
-        'HOST': 'localhost',  # set in docker-compose.yml
-        'PORT': 3306  # default postgres port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'database.sqlite'),
     }
 }
